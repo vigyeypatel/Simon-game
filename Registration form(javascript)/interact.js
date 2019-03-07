90caa217b445fad16106fa0f6ae3222aba6495f9
vigyey <vigyeypatel@gmail.com>
2019-03-07 19:37:43 +0530

    code

---
+++ Registration form(javascript)/interact.js
@@ -0,0 +1,22 @@
+/* function addRow()
+{
+    var name =document.getElementById('name').value;
+    var age=document.getElementById('age').value;
+    var gender=document.getElementById('gender').value;
+    var courses=document.getElementById('courses').value;
+
+    var table= document.getElementsByTagName('table')[1];
+
+    var newRow = table.insertRow(0);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    
+    cell1.innerHTML = fname;
+    cell2.innerHTML = age;
+    cell3.innerHTML = gender;
+    cell4.innerHTML = courses;
+}
+*/
